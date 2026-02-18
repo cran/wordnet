84fd0287758df505912e75822ba4d1bf7f2d2300
Kurt Hornik <Kurt.Hornik@R-project.org>
2026-02-18 10:03:17 +0000

    version 0.1-18

---
+++ inst/REFERENCES.R
@@ -0,0 +1,16 @@
+bibentry("Manual",
+         key = "Wallace:2007",
+         title = "Jawbone Java WordNet API",
+         author = person("Mike", "Wallace"),
+         year = "2007",
+         url = "https://sites.google.com/site/mfwallace/jawbone"
+         )
+
+bibentry("Book",
+         key = "Fellbaum:1998",
+         title = "WordNet: An Electronic Lexical Database",
+         author = person("Christiane", "Fellbaum"),
+         year = "1998",
+         publisher = "Bradford Books",
+         url = "https://mitpress.mit.edu/9780262561167/"
+         )
